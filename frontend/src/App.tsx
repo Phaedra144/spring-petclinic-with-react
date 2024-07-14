2eef9ac22d910e354422778736bd7bbb09eb8852
Phaedra144 <bedi.szilvia@gmail.com>
2024-07-14 20:36:33 +0200

    Create error page and containerize app components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,6 @@
 import { Route, Routes } from 'react-router-dom';
 import { Home } from './pages/Home';
+import { Error } from './pages/Error';
 import { Veterinarians } from './pages/Veterinarians';
 import { NavigationBar } from './navbar/NavigationBar';
 
@@ -7,9 +8,10 @@ function App() {
   return (
     <>
       <NavigationBar />
-      <div className="m-5">
+      <div className="container xd-container">
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/oups" element={<Error />} />
           <Route path="/home" element={<Home />} />
           <Route path="/vets" element={<Veterinarians />} />
         </Routes>
