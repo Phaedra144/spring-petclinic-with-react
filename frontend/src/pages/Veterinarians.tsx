204771c26631609f199af94a298952804f296015
Phaedra144 <bedi.szilvia@gmail.com>
2024-07-16 14:17:12 +0200

    Finish dynamic list with example date

--- frontend/src/pages/Veterinarians.tsx
+++ frontend/src/pages/Veterinarians.tsx
@@ -1,7 +1,38 @@
+import styles from '/src/pages/Veterinarians.module.scss';
+
+const EXAMPLE_VETS_DATA = [
+  { firstName: 'Vanessa', lastName: 'Hudgens', specialties: [{ name: 'separation syndrome' }] },
+  { firstName: 'Theodor', lastName: 'Roosevelt', specialties: [{ name: 'liver surgery' }] },
+  { firstName: 'John', lastName: 'Connor', specialties: [] },
+];
+
 export const Veterinarians = () => {
-    return (
-      <>
-        <h2>Veterinarians</h2>
-      </>
-    );
-  };
+  return (
+    <>
+      <h2 className="m-4">Veterinarians</h2>
+      <table id="vets" className="table table-striped m-4">
+        <thead className={styles.tableHead}>
+          <tr>
+            <th>Name</th>
+            <th>Specialties</th>
+          </tr>
+        </thead>
+        <tbody>
+          {EXAMPLE_VETS_DATA.map((vet) => {
+            return (
+              <tr key={`${vet.firstName + ' ' + vet.lastName}`}>
+                <td>{`${vet.firstName + ' ' + vet.lastName}`}</td>
+                <td>
+                  {vet.specialties.map((specialty) => {
+                    return <span>{specialty.name}</span>;
+                  })}
+                  {vet.specialties.length < 1 && <span>none</span>}
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </>
+  );
+};
