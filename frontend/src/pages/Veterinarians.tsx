50db83d845aff97f294e27b6ae918fe773ef7101
Phaedra144 <bedi.szilvia@gmail.com>
2024-07-16 16:21:38 +0200

    Add custom hook paging with somewhat ok design

--- frontend/src/pages/Veterinarians.tsx
+++ frontend/src/pages/Veterinarians.tsx
@@ -1,12 +1,16 @@
+import { usePaging } from '../hooks/usePaging';
 import styles from '/src/pages/Veterinarians.module.scss';
 
 const EXAMPLE_VETS_DATA = [
   { firstName: 'Vanessa', lastName: 'Hudgens', specialties: [{ name: 'separation syndrome' }] },
   { firstName: 'Theodor', lastName: 'Roosevelt', specialties: [{ name: 'liver surgery' }] },
   { firstName: 'John', lastName: 'Connor', specialties: [] },
+  { firstName: 'Stewart', lastName: 'Harold', specialties: [] },
+  { firstName: 'Santana', lastName: 'Jacobson', specialties: [] },
 ];
 
 export const Veterinarians = () => {
+  const [currentItems, paginator] = usePaging({ itemsPerPage: 2, items: EXAMPLE_VETS_DATA });
   return (
     <>
       <h2 className="mb-2">Veterinarians</h2>
@@ -18,21 +22,25 @@ export const Veterinarians = () => {
           </tr>
         </thead>
         <tbody>
-          {EXAMPLE_VETS_DATA.map((vet) => {
-            return (
-              <tr key={`${vet.firstName + ' ' + vet.lastName}`}>
-                <td>{`${vet.firstName + ' ' + vet.lastName}`}</td>
-                <td>
-                  {vet.specialties.map((specialty) => {
-                    return <span>{specialty.name}</span>;
-                  })}
-                  {vet.specialties.length < 1 && <span>none</span>}
-                </td>
-              </tr>
-            );
-          })}
+          {Array.isArray(currentItems) &&
+            currentItems.map(
+              (vet: { firstName: string; lastName: string; specialties: { name: string }[] }) => {
+                return (
+                  <tr key={`${vet.firstName + ' ' + vet.lastName}`}>
+                    <td>{`${vet.firstName + ' ' + vet.lastName}`}</td>
+                    <td>
+                      {vet.specialties.map((specialty) => {
+                        return <span>{specialty.name}</span>;
+                      })}
+                      {vet.specialties.length < 1 && <span>none</span>}
+                    </td>
+                  </tr>
+                );
+              },
+            )}
         </tbody>
       </table>
+      {paginator}
     </>
   );
 };
