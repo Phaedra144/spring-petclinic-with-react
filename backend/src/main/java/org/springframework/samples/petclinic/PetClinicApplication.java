9c7bc903b80ccf1dbccec891795e5d97d3962c64
Phaedra144 <bedi.szilvia@gmail.com>
2024-07-29 11:54:03 +0200

    Add general cors registry for testing purposes

--- backend/src/main/java/org/springframework/samples/petclinic/PetClinicApplication.java
+++ backend/src/main/java/org/springframework/samples/petclinic/PetClinicApplication.java
@@ -20,9 +20,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ImportRuntimeHints;
-import org.springframework.web.servlet.i18n.SessionLocaleResolver;
-
-import java.util.Locale;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 /**
  * PetClinic Spring Boot Application.
@@ -36,6 +35,21 @@ public class PetClinicApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PetClinicApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+					.allowedOrigins("*")
+					.allowedHeaders("*")
+					.allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD")
+					.maxAge(-1) // add maxAge
+					.allowCredentials(false);
+			}
+		};
 	}
 
 }
